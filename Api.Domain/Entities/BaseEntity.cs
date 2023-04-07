82c919905703f3cdaf96f145f6041ae96bbbcbcc
Cristiano Gehring <cristiano.gehring@gmail.com>
2023-04-07 18:40:59 -0300

    created the UserEntity

--- Api.Domain/Entities/BaseEntity.cs
+++ Api.Domain/Entities/BaseEntity.cs
@@ -9,7 +9,7 @@ namespace Api.Domain.Entities
         public Guid Id { get; set; }
 
         private DateTime? _createAt;
-        public DateTime CreateAt
+        public DateTime? CreateAt
         {
             get { return _createAt; }
             set { _createAt = (value == null ? DateTime.UtcNow : value); }
